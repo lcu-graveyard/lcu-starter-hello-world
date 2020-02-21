d1bdf996c5f6846a03342b98818a505526247bbb
Robert Wise <robert.wise@fathym.com>
2020-02-21 14:42:50 -0700

    Initial LCU structure generated

---
+++ projects/demo/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FathymSharedModule, MaterialModule } from '@lcu/common';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './controls/home/home.component';
+import { LcuStarterHelloWorldModule } from '@fathym-it/lcu-starter-hello-world-common';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FathymSharedModule,
+    MaterialModule,
+    FlexLayoutModule,
+    LcuStarterHelloWorldModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  exports: [LcuStarterHelloWorldModule]
+})
+export class AppModule { }
